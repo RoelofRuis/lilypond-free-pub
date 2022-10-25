298d7bc7d507cfbe7095ef90edd29e26e1635119
Roelof Ruis <roelof@maester.com>
2022-10-26 01:31:36 +0200

    publish now builds from music folder so it correctly includes files from that folder

--- publish.sh
+++ publish.sh
@@ -4,14 +4,20 @@ mkdir -p pub
 
 TAG=$1
 
+ROOTDIR=$(pwd)
+
 for file in $(find "music" -type f -name '*.ly')
 do
-	BASE=$(basename -s .ly $file)
-	DATE=$(date +%Y%m)
-	if [ -z "$1" ]; then
-		OUT="pub/${BASE}_${DATE}"
-	else
-		OUT="pub/${BASE}_${DATE}_${TAG}"
-	fi
-	lilypond -dno-point-and-click -drelative-includes -o $OUT $file
+    FILEDIR=$(dirname $file)
+	  BASE=$(basename -s .ly $file)
+	  DATE=$(date +%Y%m)
+	  echo `pwd`
+    cd "./${FILEDIR}"
+	  if [ -z "$1" ]; then
+		  OUT="${ROOTDIR}/pub/${BASE}_${DATE}"
+	  else
+		  OUT="${ROOTDIR}/pub/${BASE}_${DATE}_${TAG}"
+	  fi
+	  lilypond -dno-point-and-click -drelative-includes -o $OUT $BASE
+    cd "${ROOTDIR}"
 done
