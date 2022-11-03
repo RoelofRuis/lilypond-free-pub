ad50494575a9c1e9629bf235de978853f96773f2
Roelof Ruis <roelof.ruis@gmail.com>
2022-11-04 00:43:38 +0100

    include latest published files in the repository

--- publish.sh
+++ publish.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-mkdir -p pub
-
 TAG=$1
-
 ROOTDIR=$(pwd)
 
 for file in $(find "music" -type f -name '*.ly')
 do
     FILEDIR=$(dirname $file)
-	  BASE=$(basename -s .ly $file)
-	  DATE=$(date +%Y%m)
-	  echo `pwd`
+    BASE=$(basename -s .ly $file)
+    echo `pwd`
     cd "./${FILEDIR}"
-	  if [ -z "$1" ]; then
-		  OUT="${ROOTDIR}/pub/${BASE}_${DATE}"
-	  else
-		  OUT="${ROOTDIR}/pub/${BASE}_${DATE}_${TAG}"
-	  fi
-	  lilypond -dno-point-and-click -drelative-includes -o $OUT $BASE
+    if [ -z "$1" ]; then
+        OUT="${ROOTDIR}/pub/${BASE}"
+    else
+        OUT="${ROOTDIR}/pub/${BASE}_${TAG}"
+    fi
+    lilypond -dno-point-and-click -drelative-includes -o $OUT $BASE
     cd "${ROOTDIR}"
 done
+
+DATE=$(date +'%A %B %d, %Y - %H:%M')
+sed -i "s#<span id=\"build-time\">[^<]*</span>#<span id=\"build-time\">$DATE</span>#" index.html
+echo "Updated new publish time to $DATE"
